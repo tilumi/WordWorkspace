91a9c6d78694100479c4faa8ffd1476358eb1bab
DR <tw.julu@gmail.com>
2012-05-08 13:28:01 +0800

    開始啟用HTML5

--- layout_admin/tpl_header.php
+++ layout_admin/tpl_header.php
@@ -123,7 +123,7 @@ $mainmenu_for_layout=$tmp;
 $submenu_for_layout=$_;
 
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE HTML>
 <html xmlns="http://www.w3.org/1999/xhtml"><head>
 
 <?php View::include_http_metas(); ?>
