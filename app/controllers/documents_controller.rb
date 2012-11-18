b63488ab8b1cc0f64bd0b8a5b4b6dfd0d538d858
tilumi <tilumi0@gmail.com>
2012-11-18 15:32:36 +0800

    Comlete markup func & added border layout

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -18,35 +18,8 @@ class DocumentsController < ApplicationController
   def show
     content = File.read(Document.find(params[:id]).filepath, :mode => "r", :encoding => "Big5")
     parsed_content=Nokogiri::HTML(content)
-
-    js_elem = Nokogiri::XML::Node.new "script", parsed_content
-    js_elem["src"] = "/assets/jquery.js?body=1"
-    js_elem["type"] = "text/javascript"
-    parsed_content.css("head").first << js_elem
-
-    js_elem = Nokogiri::XML::Node.new "script", parsed_content
-    js_elem["src"] = "/assets/jquery_ujs.js?body=1"
-    js_elem["type"] = "text/javascript"
-    parsed_content.css("head").first << js_elem
-
-    js_elem = Nokogiri::XML::Node.new "script", parsed_content
-    js_elem["src"] = "/assets/rangy-core.js?body=1"
-    js_elem["type"] = "text/javascript"
-    parsed_content.css("head").first << js_elem
-
-    js_elem = Nokogiri::XML::Node.new "script", parsed_content
-    js_elem["src"] = "/assets/document.js?body=1"
-    js_elem["type"] = "text/javascript"
-    parsed_content.css("head").first << js_elem
-
-    css_elem = Nokogiri::XML::Node.new "link", parsed_content
-    css_elem["href"] = "/assets/document.css?body=1"
-    css_elem["media"] = "all"
-    css_elem["rel"] = "stylesheet"
-    css_elem["type"] = "text/css"
-    parsed_content.css("head").first << css_elem
-
-    render :text => parsed_content.to_html
+    @body = parsed_content.css("body").children.to_html.html_safe
+    @title = parsed_content.css("head title").children[0].to_str
   end
 
   def destroy
